10b4cb0dbbadfa12ab88dc6be6c80d44a3c32cc0
Maurice Sandoval <m239111@gmail.com>
2016-01-27 14:00:56 -0800

    Create PythonDrill_PartThree_1_SQLDatabases.py

---
+++ Python/PythonDay2/PythonDrill_PartThree_1_SQLDatabases.py
@@ -0,0 +1,27 @@
+##########
+########## Creating database and entering data
+##########
+
+import sqlite3
+
+# Connect to database 'simpsons.db'
+conn = sqlite3.connect('simpsons.db')
+
+
+# Create table named SIMPSON_INFO
+conn.execute("CREATE TABLE SIMPSON_INFO( \
+ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+NAME TEXT, \
+GENDER TEXT, \
+AGE INT, \
+OCCUPATION TEXT \
+);")
+
+
+# Add Bart Simpson to table
+conn.execute("INSERT INTO SIMPSON_INFO \
+(NAME, GENDER, AGE, OCCUPATION) VALUES \
+('Bart Simpson', 'Male', 10, 'Student')");
+
+#Save Changes
+conn.commit
