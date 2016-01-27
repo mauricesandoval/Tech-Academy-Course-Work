19c95e567bb01d0913d91ce3207120a8c3ff4131
Maurice Sandoval <m239111@gmail.com>
2016-01-27 15:00:09 -0800

    Update PythonDrill_PartThree_8_SQLDatabases.py

--- Python/PythonDay2/PythonDrill_PartThree_8_SQLDatabases.py
+++ Python/PythonDay2/PythonDrill_PartThree_8_SQLDatabases.py
@@ -1,1 +1,26 @@
+#####
+#####  Updating Data in the Database
+#####
+
+import sqlite3
+
+# Connect to database 'simpsons.db'
+conn = sqlite3.connect('simpsons.db')
+
+# Make Homer Simpson's age 41
+conn.execute("UPDATE SIMPSON_INFO set AGE=41 where NAME='Homer Simpson'")
+
+# Save changes
+conn.commit()
+
+# Print number of changes
+changes = conn.total_changes
+print "Number of changes: ",changes
+
+# Get data from database
+cursor = conn.execute("SELECT * from SIMPSON_INFO")
+
+# Extract data from cursor
+rows = cursor.fetchall()
+print rows
 
