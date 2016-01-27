75aa014a6d49ed53fb4db80d4611f73b480851ed
Maurice Sandoval <m239111@gmail.com>
2016-01-27 14:42:39 -0800

    Create PythonDrill_PartThree_4_ShellOutput.py

---
+++ Python/PythonDay2/PythonDrill_PartThree_4_ShellOutput.py
@@ -0,0 +1,21 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Python27\pythonDay2Databases.py ================
+
+Traceback (most recent call last):
+  File "C:\Python27\pythonDay2Databases.py", line 14, in <module>
+    );")
+OperationalError: table SIMPSON_INFO already exists
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases2.py ================
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases2.py ================
+Number of changes: 1
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases3.py ================
+Number of changes: 2
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases4.py ================
+<sqlite3.Cursor object at 0x029D6CE0>
+>>> 
