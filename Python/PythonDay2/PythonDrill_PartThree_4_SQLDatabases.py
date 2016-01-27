1fa9bf7f81ebc18f8d95814b4a0d3c035d306e3c
Maurice Sandoval <m239111@gmail.com>
2016-01-27 14:41:57 -0800

    Update PythonDrill_PartThree_4_SQLDatabases.py

--- Python/PythonDay2/PythonDrill_PartThree_4_SQLDatabases.py
+++ Python/PythonDay2/PythonDrill_PartThree_4_SQLDatabases.py
@@ -1,1 +1,9 @@
+import sqlite3
+
+# Connect to database 'simpsons.db'
+conn = sqlite3.connect('simpsons.db')
+
+# Get data from database
+cursor = conn.execute("SELECT ID, NAME, GENDER, AGE, OCCUPATION from SIMPSON_INFO")
+print cursor
 
