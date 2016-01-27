4bd7af406d39f010502ff9d1eb9c3da3fe780672
Maurice Sandoval <m239111@gmail.com>
2016-01-27 14:33:32 -0800

    Create PythonDrill_PartThree_2_SQLDatabases.py

---
+++ Python/PythonDay2/PythonDrill_PartThree_2_SQLDatabases.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+# Connect to database 'simpsons.db'
+conn = sqlite3.connect('simpsons.db')
+
+# Add Bart Simpson to table
+conn.execute("INSERT INTO SIMPSON_INFO \
+    (NAME, GENDER, AGE, OCCUPATION) VALUES \
+    ('Bart Simpson', 'Male', 10, 'Student' )");
+
+# Save changes
+conn.commit()
