6da099fab8daa542fad73364f5e7af3c1a0984cd
Maurice Sandoval <m239111@gmail.com>
2016-01-27 15:00:37 -0800

    Update PythonDrill_PartThree_8_ShellOutput.py

--- Python/PythonDay2/PythonDrill_PartThree_8_ShellOutput.py
+++ Python/PythonDay2/PythonDrill_PartThree_8_ShellOutput.py
@@ -1,1 +1,39 @@
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Python27\pythonDay2Databases.py ================
 
+Traceback (most recent call last):
+  File "C:\Python27\pythonDay2Databases.py", line 14, in <module>
+    );")
+OperationalError: table SIMPSON_INFO already exists
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases2.py ================
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases2.py ================
+Number of changes: 1
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases3.py ================
+Number of changes: 2
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases4.py ================
+<sqlite3.Cursor object at 0x029D6CE0>
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases5.py ================
+[(1, u'Homer Simpson', u'Male', 40, u'Nuclear Plant'), (2, u'Lisa Simpson', u'Female', 8, u'Student')]
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases6.py ================
+Results for Homer Simpson:
+[(1, u'Homer Simpson', u'Male', 40, u'Nuclear Plant')]
+Results for Females:
+[(2, u'Lisa Simpson', u'Female', 8, u'Student')]
+Results for Students:
+[(2, u'Lisa Simpson', u'Female', 8, u'Student')]
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases7.py ================
+Number of changes:  1
+[(1, u'Homer Simpson', u'Male', 40, u'Nuclear Plant')]
+>>> 
+================ RESTART: C:/Python27/pythonDay2Databases8.py ================
+Number of changes:  1
+[(1, u'Homer Simpson', u'Male', 41, u'Nuclear Plant')]
+>>> 
