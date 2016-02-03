fc7a34cef639d29ed0ae4dd9ef52d8df9de0de2e
marz sand <m239111@gmail.com>
2016-02-02 21:47:36 -0800

    Adding File
    Python CSV Drill

---
+++ Python/PythonDay2/PythonCSV/pythonDay2CSV2.py
@@ -0,0 +1,24 @@
+import os, csv
+
+# The path to the script
+currentPath = os.path.dirname(os.path.abspath("__file__"))
+
+# Make the spreadsheet path
+inputCsv = currentPath + '/spreadsheet.csv'
+
+# Open file in read mode
+csvFile = open(inputCsv, "rb")
+
+# Create reader object
+reader = csv.reader(csvFile, delimiter=',')
+'''
+# Print out data in file
+for row in reader:
+    print row
+'''
+# Add data to array
+readerData = []
+for row in reader:
+    readerData.append(row)
+    
+print readerData
