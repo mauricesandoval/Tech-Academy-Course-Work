c24116b67785e9526c1b03cf08e83593b6f33cf2
marz sand <m239111@gmail.com>
2016-02-02 21:46:45 -0800

    Adding File
    Python CSV Drill

---
+++ Python/PythonDay2/PythonCSV/pythonDay2CSV.py
@@ -0,0 +1,39 @@
+import os, csv
+
+#The path to the script
+currentPath = os.path.dirname(\
+    os.path.abspath("__file__"))
+print currentPath 
+
+#Make the spreadsheet path 
+outputCsv = currentPath + '/spreadsheet.csv'
+print outputCsv
+
+
+#Open the file
+csvFile = open(outputCsv, "wb")
+
+#Writing to the file
+#csvFile.write('Testing on Friday again')
+#csvFile.close()
+
+
+# Create writer object
+writer = csv.writer(csvFile, delimiter=',')
+
+# Data to go in csv
+row_1 = [1, "Row 1", 123]
+row_2 = [2, "Row 2", 456]
+row_3 = [3, "Row 3", 789]
+
+'''#Write rows to csv
+writer.writerow(row_1)
+writer.writerow(row_2)
+writer.writerow(row_3)'''
+
+# All rows
+rows = [row_1, row_2, row_3]
+
+# Loop rows and write each
+for row in rows:
+    writer.writerow(row)
