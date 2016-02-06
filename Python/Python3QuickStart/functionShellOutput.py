7605e356a639b858632603e46e4bc2bbb4f6eb32
marz sand <m239111@gmail.com>
2016-02-05 19:04:43 -0800

    Adding Files
    function

---
+++ Python/Python3QuickStart/functionShellOutput.py
@@ -0,0 +1,24 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\02 Quick Start\function.py 
+1 is special
+2 is a prime number
+3 is a prime number
+4 equals 2 x 2
+5 is a prime number
+6 equals 2 x 3
+7 is a prime number
+8 equals 2 x 4
+9 equals 3 x 3
+10 equals 2 x 5
+11 is a prime number
+12 equals 2 x 6
+13 is a prime number
+14 equals 2 x 7
+15 equals 3 x 5
+16 equals 2 x 8
+17 is a prime number
+18 equals 2 x 9
+19 is a prime number
+>>> 
