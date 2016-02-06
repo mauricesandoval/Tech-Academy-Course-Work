e9bd5483047e37230b2d14d3f3b1456b277a7b41
marz sand <m239111@gmail.com>
2016-02-05 19:07:57 -0800

    Adding Files
    Hello World

---
+++ Python/Python3QuickStart/helloShellOutput.py
@@ -0,0 +1,6 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\02 Quick Start\hello.py 
+Hello, World!
+>>> 
