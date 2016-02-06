864e8f2e8b3239bd69c24e9711d907795ce62771
marz sand <m239111@gmail.com>
2016-02-05 19:12:00 -0800

    Adding Files
    object-oriented programming 3

---
+++ Python/Python3QuickStart/oop3ShellOutput.py
@@ -0,0 +1,19 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\02 Quick Start\oop3.py 
+-- In the forest:
+Quaaaaak!
+The duck has gray and white feathers.
+The person imitates a duck.
+The person takes a feather from the ground and shows it.
+The dog has no quack
+The dog has no feathers
+-- In the doghouse:
+The duck has no bark
+The duck has no fur
+The person says woof!
+The person puts on a fur coat.
+Arf!
+The dog has white fur with black spots.
+>>> 
