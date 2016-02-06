c403f2389d7a51e8acee2f8bc155abde08986c09
marz sand <m239111@gmail.com>
2016-02-05 18:59:06 -0800

    Adding Files
    conditionals

---
+++ Python/Python3QuickStart/conditionalsShellOutput.py
@@ -0,0 +1,11 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\02 Quick Start\conditionals.py 
+a (0) is less than b (1)
+foo
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\02 Quick Start\conditionals.py 
+a (5) is not less than b (1)
+bar
+>>> 
