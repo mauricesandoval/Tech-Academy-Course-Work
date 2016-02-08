aa9fc9732e4399091b1480c17b93fb9e9fcc4c01
marz sand <m239111@gmail.com>
2016-02-07 20:20:45 -0800

    Adding Files
    Python 3 syntax

---
+++ Python/Python3Drills/syntaxShellOutput.py
@@ -0,0 +1,25 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+This is the syntax.py file.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+This is the syntax.py file.
+egg
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+This is the syntax.py file.
+Traceback (most recent call last):
+  File "C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py", line 8, in <module>
+    egg()
+NameError: name 'egg' is not defined
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\syntax.py 
+This is the syntax.py file.
+egg
+>>> 
