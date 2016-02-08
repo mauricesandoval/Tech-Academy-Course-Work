3771e24eac31a6ff1733d9c263ef9d7e7f16c145
Maurice Sandoval <m239111@gmail.com>
2016-02-07 20:26:37 -0800

    Update syntax.py

--- Python/Python3Drills/syntax.py
+++ Python/Python3Drills/syntax.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# syntax.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
+
 
 def main():
     print("This is the syntax.py file.")
