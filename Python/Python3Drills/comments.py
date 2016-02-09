bbaafee7e589b167a65ddc8368a7eb641148f44b
marz sand <m239111@gmail.com>
2016-02-08 21:13:09 -0800

    Adding Files
    Comments in Python3

---
+++ Python/Python3Drills/comments.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def main():
+    for n in primes(): # generate a list of prime numbers
+        if n > 100: break
+        print(n)
+
+def isprime(n):
+    if n == 1: # one is never prime
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False # found a divisor, not prime
+    else:
+        return True
+
+def primes(n = 1):
+   while(True):
+       if isprime(n): yield n #yield makes this a generator
+       n += 1 
+
+if __name__ == "__main__": main()
---
+++ Python/Python3Drills/commentsShellOutput.py
@@ -0,0 +1,30 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\04 Syntax\comments.py 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+>>> 
