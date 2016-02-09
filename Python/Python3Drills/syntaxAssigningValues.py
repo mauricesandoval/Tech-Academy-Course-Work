ea5bc20bc9689590a8d5dc19565ae1a25f1ea043
marz sand <m239111@gmail.com>
2016-02-08 21:18:26 -0800

    Adding Files
    Assigning Values

---
+++ Python/Python3Drills/syntaxAssigningValues.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+# Assigning Values
+
+# The assignment opertor is used for assigning a value in Python and
+# it will also create an object if that has not already been created
+
+def main():
+    a = 1
+    print(a)
+
+if __name__ == "__main__": main()
+
+
+# 'one' spelled out
+def main():
+    a = "one"
+    print(a)
+
+if __name__ == "__main__": main()
+
+
+# use type function to print the type and value - string
+def main():
+    a = "one"
+    print(type(a),a) #use type() function. Type of thing is usually 
+                     #the name of the class
+if __name__ == "__main__": main()
+
+
+
+# use type function to print the type and value - integer
+def main():
+    a = 7
+    print(type(a),a)
+    
+if __name__ == "__main__": main()
+
+
+# use with two values
+def main():
+    a, b = 0,1
+    print(a,b)
+    
+if __name__ == "__main__": main()
+
+#
+def main():
+    a, b = 0, 1
+    a, b = b, a  # swaps values
+    print(a, b)
+    
+if __name__ == "__main__": main()
+
+
+# Tuples
+def main():
+    a = (1,2,3,4,5) # Parenthesis
+    print(a)
+    
+if __name__ == "__main__": main()
+
+
+# List
+def main():
+    a = [1,2,3,4,5]  # square brackets
+    print(a)
+    
+if __name__ == "__main__": main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/Python3Drills/syntaxAssigningValuesShellOutput.py
@@ -0,0 +1,68 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+<class 'int'> 7
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+0 1
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+7
+0 1
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+<class 'int'> 7
+0 1
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+<class 'int'> 7
+0 1
+1 0
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+<class 'int'> 7
+0 1
+1 0
+(1, 2, 3, 4, 5)
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntaxAssigningValues.py 
+1
+one
+<class 'str'> one
+<class 'int'> 7
+0 1
+1 0
+(1, 2, 3, 4, 5)
+[1, 2, 3, 4, 5]
+>>> 
