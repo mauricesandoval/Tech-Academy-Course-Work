a25c0ad2b76a4712a36c7fd187cf45a9893915c5
marz sand <m239111@gmail.com>
2016-02-11 23:53:51 -0800

    Adding Files

---
+++ Python/Python3Conditionals/conditionals-IfElse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# This is an exercise file from Python 3 Essential Training on lynda.com
+
+# Conditionals - If - Else statements
+
+def main():
+    a, b = 0, 1
+    if a < b:
+        print("this is true")
+        
+if __name__ == "__main__": main()
+
+
+
+# change values and add else()
+
+def main():
+    a, b = 1, 1
+    if a < b:
+        print('this is true')
+    else:
+        print('this is not true')
+        
+if __name__ == "__main__": main()
+
+
+
+# change if statement
+
+def main():
+    a, b = 1, 1
+    if False:
+        print('this is true')
+    else:
+        print('this is not true')
+        
+if __name__ == "__main__": main()
---
+++ Python/Python3Conditionals/conditionals-IfElseOutputShell.py
@@ -0,0 +1,12 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-IfElse.py 
+this is true
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-IfElse.py 
+this is not true
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-IfElse.py 
+this is not true
+>>> 
