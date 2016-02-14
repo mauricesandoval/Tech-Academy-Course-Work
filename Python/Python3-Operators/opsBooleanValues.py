d23f327568c8a69da8823ae3c3f98f104a43c19d
marz sand <m239111@gmail.com>
2016-02-13 21:08:05 -0800

    Adding Files

---
+++ Python/Python3-Operators/opsBooleanValues.py
@@ -0,0 +1,29 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 5 == 5
+True
+>>> 7 < 5
+False
+>>> type(True)
+<class 'bool'>
+>>> True and False
+False
+>>> True and True
+True
+>>> True or False
+True
+>>> False or False
+False
+>>> True & True
+True
+>>> a, b = 0, 1
+>>> x, y = 'zero', 'one'
+>>> x < y
+False
+>>> a < b
+True
+>>> if a < b and x < y: print('yes')
+else: print('no')
+
+no
+>>> 
