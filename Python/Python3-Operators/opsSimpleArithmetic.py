4b5b3cd830395ae54777be6542176da034081d98
marz sand <m239111@gmail.com>
2016-02-13 21:09:17 -0800

    Adding files

---
+++ Python/Python3-Operators/opsSimpleArithmetic.py
@@ -0,0 +1,33 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 5 + 5
+10
+>>> 5 * 5
+25
+>>> 5 - 3
+2
+>>> 5 / 3
+1.6666666666666667
+>>> 5 // 3
+1
+>>> 5 % 3
+2
+>>> divmod(5,3)
+(1, 2)
+>>> num = 5
+>>> num += 1
+>>> num
+6
+>>> num -= 1
+>>> num
+5
+>>> num *= 5
+>>> num
+25
+>>> num //= 5
+>>> num
+5
+>>> num /= 5
+>>> num
+1.0
+>>> 
