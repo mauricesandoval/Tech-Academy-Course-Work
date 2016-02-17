8369c57f6a0b7a43b78c335c6e70fe94ffbc2d4b
marz sand <m239111@gmail.com>
2016-02-16 22:47:54 -0800

    Adding Files

---
+++ Python/Python3-Exceptions/exceptionsHandlingShellOutput.py
@@ -0,0 +1,47 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\Desktop\Ex_Files_Python_3_EssT\Exercise Files\10 Exceptions\exceptions.py 
+01 This is a line of text
+02 This is a line of text
+03 This is a line of text
+04 This is a line of text
+05 This is a line of text
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+Traceback (most recent call last):
+  File "C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py", line 22, in <module>
+    if __name__ == "__main__": main()
+  File "C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py", line 19, in main
+    fh = open('xlines.txt')
+FileNotFoundError: [Errno 2] No such file or directory: 'xlines.txt'
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+could not open the file. come back tomorrow
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+could not open the file. come back tomorrow
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+01 This is a line of text
+02 This is a line of text
+03 This is a line of text
+04 This is a line of text
+05 This is a line of text
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+could not open the file. come back tomorrow
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+could not open the file: [Errno 2] No such file or directory: 'xlines.txt'
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+could not open the file: [Errno 2] No such file or directory: 'xlines.txt'
+>>> 
+ RESTART: C:/Users/Student/Desktop/Ex_Files_Python_3_EssT/Exercise Files/10 Exceptions/exceptionsHandling.py 
+01 This is a line of text
+02 This is a line of text
+03 This is a line of text
+04 This is a line of text
+05 This is a line of text
+>>> 
