30a11fae4e9924e63ce3004834ea050fb2b4b3a6
marz sand <m239111@gmail.com>
2016-02-23 20:20:12 -0800

    Adding File

---
+++ Python/Python3-Containers/containersBytesByteArraysShell.py
@@ -0,0 +1,13 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python35-32/Ex_Files_Python_3_EssT/Exercise Files/14 Containers/containersBytesByteArrays.py 
+This is the containers.py file.
+>>> 
+ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python35-32/Ex_Files_Python_3_EssT/Exercise Files/14 Containers/containersBytesByteArrays.py 
+This is a UTF-8 file. 
+It has some interesting characters in it. 
+&#1641;(&#0865;&#3663;&#0815;&#0865;&#3663;)&#1782;
+
+Done.
+>>> 
