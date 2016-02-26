5f8edc177b318527aa8dd998353004f9f6be8ef6
marz sand <m239111@gmail.com>
2016-02-26 09:15:43 -0800

    Adding Files

---
+++ Python/Python3-Databases/sqlite3-classOutput.py
@@ -0,0 +1,21 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Student\AppData\Local\Programs\Python\Python35-32\Ex_Files_Python_3_EssT\Exercise Files\16 Databases\sqlite3-class.py 
+Create table test
+Create rows
+{'t1': 'four', 'i1': 4}
+{'t1': 'one', 'i1': 1}
+{'t1': 'three', 'i1': 3}
+{'t1': 'two', 'i1': 2}
+Retrieve rows
+{'t1': 'one', 'i1': 1} {'t1': 'two', 'i1': 2}
+Update rows
+{'t1': 'four', 'i1': 4}
+{'t1': 'one', 'i1': 101}
+{'t1': 'three', 'i1': 103}
+{'t1': 'two', 'i1': 2}
+Delete rows
+{'t1': 'four', 'i1': 4}
+{'t1': 'two', 'i1': 2}
+>>> 
