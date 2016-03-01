9bcc40daee3329de2e41d88ba39056db0f4569c1
marz sand <m239111@gmail.com>
2016-02-29 22:50:01 -0800

    Adding Files

---
+++ Python/Python3-Debugging/RuntimeErrorsOutput.py
@@ -0,0 +1,35 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python35-32/Ex_Files_Python_3_EssT/Exercise Files/18 Debugging/RuntimeErrors.py 
+-- In the forest:
+Quaaaaak!
+<bound method AnimalActions.feathers of <__main__.Duck object at 0x02E6DC10>>
+The person imitates a duck.
+<bound method AnimalActions.feathers of <__main__.Person object at 0x02E6DC30>>
+The dog has no quack
+<bound method AnimalActions.feathers of <__main__.Dog object at 0x02E6DC50>>
+-- In the doghouse:
+The duck has no bark
+The duck has no fur
+The person says woof!
+The person puts on a fur coat.
+Arf!
+The dog has white fur with black spots.
+>>> 
+ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python35-32/Ex_Files_Python_3_EssT/Exercise Files/18 Debugging/RuntimeErrors.py 
+-- In the forest:
+Quaaaaak!
+The duck has gray and white feathers.
+The person imitates a duck.
+The person takes a feather from the ground and shows it.
+The dog has no quack
+The dog has no feathers
+-- In the doghouse:
+The duck has no bark
+The duck has no fur
+The person says woof!
+The person puts on a fur coat.
+Arf!
+The dog has white fur with black spots.
+>>> 
