aeca7819a8709b6b083a39c9a96c3d97279d2ca7
mauricesandoval <m239111@gmail.com>
2016-03-06 13:36:53 -0800

    Adding Files

---
+++ Python/Tkinter/Event_Handling/02_keyboardOutput.py
@@ -0,0 +1,34 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Python35\Python3 - Tech Academy\Tkinter_Exercise Files\Ch07 - Event Handling\02_keyboard.py 
+type: 2
+widget: .
+char: a
+keysym: a
+keycode: 65
+
+type: 2
+widget: .
+char: 
+keysym: Shift_L
+keycode: 16
+
+type: 2
+widget: .
+char: 
+keysym: Shift_L
+keycode: 16
+
+type: 2
+widget: .
+char: #
+keysym: numbersign
+keycode: 51
+
+type: 2
+widget: .
+char: 
+keysym: F1
+keycode: 112
+
