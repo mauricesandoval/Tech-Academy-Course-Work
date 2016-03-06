b9afe94e759a0a98af9a33060b5115df5900d303
mauricesandoval <m239111@gmail.com>
2016-03-06 13:58:01 -0800

    Adding File

---
+++ Python/Tkinter/Building-an-Application/05_feedback_template_with_eventsOutput.py
@@ -0,0 +1,9 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Python35\Python3 - Tech Academy\Tkinter_Exercise Files\Ch08 - Building an Application\05_feedback_template_with_events.py 
+Name: maurice
+Email: maurice@gmail.com
+Comments: This is awesome!
+
+>>> 
