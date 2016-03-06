f43a28da430966db488f95730274ec66100238dd
mauricesandoval <m239111@gmail.com>
2016-03-06 13:38:52 -0800

    Adding Files

---
+++ Python/Tkinter/Event_Handling/05_multipleOutput2.py
@@ -0,0 +1,12 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Python35\Python3 - Tech Academy\Tkinter_Exercise Files\Ch07 - Event Handling\05_multiple.py 
+<1> Label
+<ButtonPress> Label
+<1> Label
+<1> Label
+<1> Label
+<ButtonPress> Label
+<ButtonPress> Label
+<ButtonPress> Label
