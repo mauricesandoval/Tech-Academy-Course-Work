11d74cd4370ae76526a4f89b4000aee7c1d9a7b1
mauricesandoval <m239111@gmail.com>
2016-09-12 23:12:48 -0700

    adding files

---
+++ C-Sharp/CSharpBasicExcercises/data.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal maxDec = decimal.MaxValue;
+            Console.WriteLine(maxDec);
+
+           // float maxFlo = float.MaxValue;
+            //Console.WriteLine(maxFlo);
+
+            float minFloat = float.MaxValue;
+            Console.WriteLine(minFloat)
+
+
+        }
+    }
+}
