c375377a2de05e7eac2de4885f8d84ed32e569d3
mauricesandoval <m239111@gmail.com>
2016-09-30 21:18:18 -0700

    Adding Files

---
+++ JavaScript/ExpandingMenu/MauriceMenu.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("li").hide();
+    $(":button").mouseenter(function () {
+        $("li").slideToggle(1000);
+    });
+    $(":button").mouseleave(function () {
+        $("li").slideToggle(1000);
+    });
+});
